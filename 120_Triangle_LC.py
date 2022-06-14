6904603fc2022177ab33ce54170af64f6cec81f9
adithyang64 <adithyang64@gmail.com>
2022-06-14 22:44:13 +0530

    https://github.com/adithyang64/LeetCodeProblems

---
+++ 120_Triangle_LC.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        ## Bottom-up Approach
+        if triangle: 
+            #print(len(triangle))
+            #print(triangle[2])
+            for i in range(len(triangle)-2, -1, -1):
+                for j in range(len(triangle[i])):
+                    triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+            return triangle[0][0]
+        else:
+            return 0
+
+    ans = minimumTotal(1,[[2],[3,4],[6,5,7],[4,1,8,3]])
+    print(ans)
+
+    # 2
+    # 3 4
+    # 6 5 7
+    # 4 1 8 7
