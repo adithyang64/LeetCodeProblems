44d3ca5af6e16ea7bf267769d8ab235207e30c24
Adithya Narayan <67658457+adithyang64@users.noreply.github.com>
2023-06-25 21:35:19 +0530

    Update 26_RemoveDuplicatesFromSortedArray_LC.py

--- 26_RemoveDuplicatesFromSortedArray_LC.py
+++ 26_RemoveDuplicatesFromSortedArray_LC.py
@@ -17,4 +17,14 @@ class Solution(object):
     x = removeDuplicates(1,[1,1,2])
     print(x)
 
+    ## Another Approach
+
+    # unique_arr = [sorted_arr[0]]
+
+    # for i in range(1, len(sorted_arr)):
+    #     if sorted_arr[i] != sorted_arr[i-1]:
+    #         unique_arr.append(sorted_arr[i])
+
+    # return unique_arr
+
 # https://leetcode.com/problems/remove-duplicates-from-sorted-array/
