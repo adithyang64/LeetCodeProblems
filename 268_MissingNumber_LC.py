9ee33e679c07e8755f0bd0e318ca9f71bb687f1c
Adithya Narayan <67658457+adithyang64@users.noreply.github.com>
2023-06-25 21:22:28 +0530

    Update 268_MissingNumber_LC.py

--- 268_MissingNumber_LC.py
+++ 268_MissingNumber_LC.py
@@ -16,4 +16,17 @@ class Solution(object):
     x=missingNumber(1,[3,0,1])
     print(x)
 
+    ## Another Approach
+
+    # s=[3,0,1]
+    # sorted_s = sorted(s)
+    # print(sorted_s)
+    
+    # for i in range(len(sorted_s)-1):
+    #     if not (sorted_s[i+1]==sorted_s[i]+1):
+    #         a=sorted_s[i]
+    #         b=sorted_s[i+1]
+    #         for j in range(a+1,b,1):
+    #             print(j)
+
     # https://leetcode.com/problems/missing-number/
